47153d756ebb70a74a13f2c27c84d67f74ed21a1
won-jae-jang <dnjswo410@naver.com>
2024-05-30 21:56:48 +0900

    init settings

---
+++ src/main/java/nuts/muzinut/MuzinutApplication.java
@@ -0,0 +1,13 @@
+package nuts.muzinut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzinutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzinutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nuts/muzinut/MuzinutApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.muzinut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzinutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

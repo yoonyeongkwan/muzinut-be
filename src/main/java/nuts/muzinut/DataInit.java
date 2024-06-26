301d3d3eabaf0070fec81d6841be064d71767de0
won-jae-jang <dnjswo410@naver.com>
2024-06-26 16:47:19 +0900

    git ignore test~

--- src/main/java/nuts/muzinut/DataInit.java
+++ src/main/java/nuts/muzinut/DataInit.java
@@ -69,7 +69,7 @@ public class DataInit {
         createAdminBoards();
     }
 
-//    @PostConstruct
+    @PostConstruct
     public void FreeBoardScenario() {
         User user1 = new User();
         user1.setNickname("tom");
